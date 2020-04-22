3f7448e72f937ab5c3e2c82e59c1ffa23ffb2740
Jason Hur <jhur008@ucr.edu>
2020-04-22 09:17:14 -0700

    First commit. Servo is moving, control is not smooth

---
+++ servo.ino
@@ -0,0 +1,41 @@
+#include <Servo.h>
+   
+Servo myservo; // servo object to control servo
+int val; // variable to read the value from the analog pin
+const int buttonPin = 12; // where button to be pressed and switch modes is located
+int buttonState = 0;
+const int ledPin = 13; // where the led pin is located
+int potPort = 1; // port connecting to potentiometeter
+
+void setup() {
+   myservo.attach(9);
+   pinMode(buttonPin, INPUT);
+   pinMode(ledPin, OUTPUT);
+}
+
+
+/*
+ * For Milestone 1 demo:
+ * Button to swap between manual mode and "input" mode.
+ * LED will light up during "input" mode.
+ * "input" mode will be set to 1 for demo. It is supposed to take in info from the servers.
+ */
+
+void loop() {
+//   mode = analogRead(
+   // manual mode:
+   buttonState = digitalRead(buttonPin);  // know the state if button is being pressed
+   if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+   }
+   else {
+    digitalWrite(ledPin, LOW);
+   }
+   val = analogRead(potPort);
+   // reads the value of the potentiometer (value between 0 and 1023)
+   val = map(val, 0, 1023, 0, 180);
+   // scale it to use it with the servo (value between 0 and 180)
+   
+   myservo.write(val); // sets the servo position according to the scaled value
+   delay(15);
+}
