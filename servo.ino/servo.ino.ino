75524da6ad32aff5e077e80b6c7a7454b2b7c6af
Jason Hur <jhur008@ucr.edu>
2020-04-22 14:27:20 -0700

    Added auto spray fcn to replicate servo ability to move in all directions

--- servo.ino/servo.ino.ino
+++ servo.ino/servo.ino.ino
@@ -2,14 +2,14 @@
    
 Servo myservo; // servo object to control servo
 int val; // variable to read the value from the analog pin
-const int buttonPin = 12; // where button to be pressed and switch modes is located
-int buttonState = 0;//LOW;//0;
 const int ledPin = 13; // where the led pin is located
-int potPort = 1; // port connecting to potentiometeter
+const int buttonPin = 12; // where button to be pressed and switch modes is located
+int buttonState = 0;
+int potPort = 0; // port connecting to potentiometeter
+int pos = 0;
 
 void setup() {
-  // Serial.begin(9600);
-   myservo.attach(9);
+   myservo.attach(11);
    pinMode(buttonPin, INPUT);
    pinMode(ledPin, OUTPUT);
 }
@@ -19,27 +19,30 @@ void setup() {
  * For Milestone 1 demo:
  * Button to swap between manual mode and "input" mode.
  * LED will light up during "input" mode.
- * "input" mode will be set to 1 for demo. It is supposed to take in info from the servers.
+ * "input" mode will move servo so it can "shoot" in a 120 degree cone.
+ * It is supposed to take in info from the servers.
  */
 
 void loop() {
-//   mode = analogRead(
-   // manual mode:
-   buttonState = digitalRead(buttonPin);  // know the state if button is being pressed
-   
-   //Serial.println(buttonState);
-
-   if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH);
+   buttonState = digitalRead(buttonPin);  // read button state
+   // auto input mode
+   if (buttonState == HIGH) { // button press
+    digitalWrite(ledPin, HIGH); // turn LED on
+    for (pos = 0; pos <= 120; pos += 1) { // goes from 0 to 120 degrees
+    myservo.write(pos); // lets servo know what position to be in
+    delay(15); // wait 15 ms for servo
+    }
+    for (pos = 120; pos >= 0; pos -= 1) { // goes from 120 to 0 degrees
+      myservo.write(pos);
+      delay(15);
+    }
    }
+   // manual mode
    else {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW); // keep LED off
+    val = analogRead(potPort); // read potentiometer value (0 to 1023)
+    val = map(val, 0, 1023, 0, 180); // Re-map val to servo (0 to 180)
+    myservo.write(val); // set servo position based off val
    }
-   val = analogRead(potPort);
-   // reads the value of the potentiometer (value between 0 and 1023)
-   val = map(val, 0, 1023, 0, 180);
-   // scale it to use it with the servo (value between 0 and 180)
-   
-   myservo.write(val); // sets the servo position according to the scaled value
    delay(15);
 }
