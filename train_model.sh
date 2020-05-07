f7cb3fa25727cbd1052cbbdc8942b689ecfde008
hsimj001 <hsimj001@ucr.edu>
2020-05-07 11:23:40 -0700

    revert last change

--- train_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Launch training:(Note: --model_dir=training/ the virtual directory can be changed in order to view multiple training graphs on TensorBoard
-
-python model_main.py --alsologtostderr --model_dir=training/ --pipeline_config_path=training/ssd_inception_v2_coco.config
-
-# Launch training graph for training in progress: (Note: --logdir=training\ virtual directory depends on what was used for training)
-
-tensorboard --logdir=training\
