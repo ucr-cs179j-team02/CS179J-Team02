fc82b8590e175b843e47f7f620374fa386d4a8fd
Jason Hur <jhur008@ucr.edu>
2020-05-12 17:44:28 -0700

    Update for demo 2

--- servo.ino/servo.ino.ino
+++ servo.ino/servo.ino.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
    
-Servo myservo; // servo object to control servo
+Servo rotateServo; // servo object to aim the spray
+Servo shootServo; // servo object to shoot the spray
 int val; // variable to read the value from the analog pin
 const int ledPin = 13; // where the led pin is located
 const int buttonPin = 12; // where button to be pressed and switch modes is located
@@ -9,7 +10,8 @@ int potPort = 0; // port connecting to potentiometeter
 int pos = 0;
 
 void setup() {
-   myservo.attach(11);
+   rotateServo.attach(10);
+   shootServo.attach(11);
    pinMode(buttonPin, INPUT);
    pinMode(ledPin, OUTPUT);
 }
@@ -25,24 +27,24 @@ void setup() {
 
 void loop() {
    buttonState = digitalRead(buttonPin);  // read button state
-   // auto input mode
+   // Automatically move servo to shoot the spray
    if (buttonState == HIGH) { // button press
     digitalWrite(ledPin, HIGH); // turn LED on
     for (pos = 0; pos <= 120; pos += 1) { // goes from 0 to 120 degrees
-    myservo.write(pos); // lets servo know what position to be in
+    rotateServo.write(pos); // lets servo know what position to be in
     delay(15); // wait 15 ms for servo
     }
     for (pos = 120; pos >= 0; pos -= 1) { // goes from 120 to 0 degrees
-      myservo.write(pos);
+      rotateServo.write(pos);
       delay(15);
     }
    }
-   // manual mode
-   else {
+   // Manually rotate servo to aim at target direction
+   //else {
     digitalWrite(ledPin, LOW); // keep LED off
     val = analogRead(potPort); // read potentiometer value (0 to 1023)
     val = map(val, 0, 1023, 0, 180); // Re-map val to servo (0 to 180)
-    myservo.write(val); // set servo position based off val
-   }
+    shootServo.write(val); // set servo position based off val
+   //}
    delay(15);
 }
