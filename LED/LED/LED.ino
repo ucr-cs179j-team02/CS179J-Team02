a159a4237fd8f2da590bc689ea2f3ace8e29ec64
Jason Hur <jhur008@ucr.edu>
2020-05-28 19:26:28 -0700

    LED danger levels for ws2812B LEDs

--- LED/LED/LED.ino
+++ LED/LED/LED.ino
@@ -1,68 +1,72 @@
-const int ledPin1 = 7;
-const int ledPin2 = 6;
-const int ledPin3 = 5;
+#include <FastLED.h>
+
+#define LED_PIN 7
+#define NUM_LEDS 10
+
+CRGB leds(NUM_LEDS);
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
 }
-
-// need a signal first from server still. This program is in preparation.
 
 void loop() {
   // temp signal value
-  int signal = 4;
-  /* 
+  int signal = 1;
+  /*
    * Danger Level: 1
    * No reaction
    */
   if (signal == 1) {
-    digitalWrite(ledPin1, LOW); 
-    digitalWrite(ledPin2, LOW); 
-    digitalWrite(ledPin3, LOW); 
+    FastLED.clear(0)
   }
-  
   /*
    * Danger Level: 2
-   * Light show to grab threat's attention that there is a boundary.
+   * Light show to grab surrounding persons' attention.
+   * Indicate that something is happening.
    */
   else if (signal == 2) {
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin3, LOW);
-    delay(600);
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, HIGH);
-    delay(600);
-    digitalWrite(ledPin2, LOW);
-    digitalWrite(ledPin3, HIGH);
-    delay(600);
+    for (int i = 0; i < 10; i++) {
+      leds[i] = CRGB(255,0,0); // red
+      FastLED.show();
+      delay(50);
+    }
+    for (int i = 0; i < 10; i++) {
+      leds[i] = CRGB(170,0,255);  // purple
+      FastLED.show();
+      delay(50);
+    }
+    for (int i = 0; i < 10; i++) {
+      leds[i] = CRGB(0,0,255);  // blue
+      FastLED.show();
+      delay(50);
+    }
+    for (int i = 9; i >= 0; i--) {
+      leds[i] = CRGB(255,0,170);  // magenta
+      FastLED.show();
+      delay(50);
+    }
   }
-
   /*
    * Danger Level: 3
-   * Emergency Warning. Lights will flash continuously.
+   * Emergency Warning. Lights will flash red continuously.
    */
   else if (signal == 3) {
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
-    delay(200);
-    digitalWrite(ledPin1, LOW); 
-    digitalWrite(ledPin2, LOW); 
-    digitalWrite(ledPin3, LOW); 
-    delay(200);
+    for (int i = 0; i < 20; i++) {
+      leds[i] = CRGB(255,0,0);
+      FastLED.show();
+    }
+    delay(500);
+    FastLED.clear();
+    delay(300);
   }
-
   /*
    * Danger Level: 4
    * Activation Mode. Lights will stay on.
    */
   else if (signal == 4) {
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
+    for (int i = 0; i < 20; i++) {
+      leds[i] = CRGB(255,255,255);  // white
+      FastLED.show();
+    }
   }
- 
 }
