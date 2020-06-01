ccbf763546ea390e5bbb7397dd5f98ead2b8b0e8
hsimj001 <hsimj001@ucr.edu>
2020-05-31 20:07:58 -0700

    refactor hat server to work with stepper motor

--- NodeMCU/src/Server/Local/hat_server/hat_server.ino
+++ NodeMCU/src/Server/Local/hat_server/hat_server.ino
@@ -1,32 +1,46 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <AccelStepper.h>
+#define IN1   D5   // IN1 is connected to NodeMCU pin D1 (GPIO5)
+#define IN2   D6   // IN2 is connected to NodeMCU pin D2 (GPIO4)
+#define IN3   D7   // IN3 is connected to NodeMCU pin D3 (GPIO0)
+#define IN4   D8   // IN4 is connected to NodeMCU pin D4 (GPIO2)
+#define MotorInterfaceType 8
 
 const char *ssid = "RonaZona_hat";
 const char *password = "squidward420";
  
 ESP8266WebServer server(80);
+AccelStepper stepper = AccelStepper(MotorInterfaceType, IN1, IN3, IN2, IN4);
 
 void handleNotFound(){
-  server.send(200, "", "");
+    server.send(200, "", "");
+    stepper.setSpeed(250);
 }
 
-void handleRequestOff(){
+void handleRequestFar(){
     server.send(200, "", "");
-    digitalWrite(D4, LOW);
+    stepper.setSpeed(250);
 }
 
-void handleRequestHat(){
+void handleRequestCloser(){
     server.send(200, "", "");
-    digitalWrite(D4, HIGH);
+    stepper.setSpeed(500);
+}
+
+void handleRequestClose(){
+    server.send(200, "", "");
+    stepper.setSpeed(750);
+}
+
+void handleRequestVeryClose(){
+    server.send(200, "", "");
+    stepper.setSpeed(1000);
 }
  
 void setup() {
-  pinMode(D4, OUTPUT);
-  
   Serial.begin(115200);
-
-  digitalWrite(D4, LOW);
  
   WiFi.softAP(ssid, password);
 
@@ -47,15 +61,20 @@ void setup() {
     Serial.println("Error setting up MDNS responder!");
   }
 
-  server.on("/off", handleRequestOff);
-  server.on("/hat", handleRequestHat);
+  server.on("/far", handleRequestFar);
+  server.on("/closer", handleRequestCloser);
+  server.on("/close", handleRequestClose);
+  server.on("/veryclose", handleRequestVeryClose);
   server.onNotFound(handleNotFound);
-  
-  server.begin();
  
+  server.begin();
   Serial.println("Server listening");
+  
+  stepper.setMaxSpeed(1000);
+  stepper.setSpeed(250);
 }
  
 void loop() {
   server.handleClient();
+  stepper.runSpeed();
 }
